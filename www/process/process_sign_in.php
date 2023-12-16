9cfe0e0e10a79e514d201027cff94bbc51ab1638
apavazza <115418293+apavazza@users.noreply.github.com>
2023-12-16 23:43:51 +0100

    implemented sign in

--- www/process/process_sign_in.php
+++ www/process/process_sign_in.php
@@ -1,17 +1,19 @@
 <?php
 	if(true){
-		$host = 'postgres';
-		$dbname = 'ecommerce';
-		$user = 'admin';
-		$password = '~a5Xf;UB}^3kchY'; //only for testing purposes, not an actual password
+		$db_host = 'postgres';
+		$db_name = 'ecommerce';
+		$db_user = 'admin';
+		$db_password = '~a5Xf;UB}^3kchY'; //only for testing purposes, not an actual password
 		
 		// Calculate password hash
 		$passwordHash = hash('sha3-256', $_POST['password']);
 		
 		try {
 			// Connect to the PostgreSQL database
-			$conn = new PDO("pgsql:host=$host;dbname=$dbname", $user, $password);
-			
+			$conn = new PDO("pgsql:host=$db_host;dbname=$db_name", $db_user, $db_password);
+			$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+
 			// Check if the connection was successful
 			if (!$conn) {
 				echo "Failed to connect to the database.";
@@ -19,8 +21,14 @@
 			}
 
 			// Prepare the SQL statement with placeholders for variables
-			$query = "SELECT password FROM customer WHERE username = 'd'";
-			$result = pg_query($conn, $query);
+			$query = "SELECT password FROM customer WHERE username = :username";
+			$stmt = $conn->prepare($query);
+			$stmt->bindValue(':username', $_POST['username']);
+			$stmt->execute();
+
+			// Fetch the result
+			$result = $stmt->fetch(PDO::FETCH_ASSOC);
+			$password = $result['password'];
 
 			// Check if the query was successful
 			if (!$result) {
@@ -28,14 +36,15 @@
 				exit;
 			}
 
-			// Close the database connection
-			pg_close($conn);
-
 			// Use the retrieved password as needed
-			echo "The password for the customer is: " . $password;
+			if($password == $passwordHash){
+				// Redirect to customer's profile
+				header("Location: /customer/profile.php"); 
+			} else{
+				echo "You have entered a wrong password";
+			}
 
-			// Redirect to customer's profile
-			header("Location: /customer/profile.php"); 
+			
 
 		} catch (PDOException $e) {
 			echo "Error: " . $e->getMessage();
